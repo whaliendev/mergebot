c8a015fd8cdec50d0a0325ca79a91c54569d8cd2
Hwa <hwahe.cs@gmail.com>
2022-11-17 22:22:42 +0800

    feat: encapsulate frequently-used cxx STL

--- include/mb/c.h
+++ include/mb/c.h
@@ -1,44 +1,41 @@
 /**
  * @file c.h
  * @author hwa (hwahe.cs@gmail.com)
- * @brief C bindings for mergebot. 
+ * @brief C bindings for mergebot.
  * @version 0.1
  * @date 2022-09-23
- * 
- * @copyright Copyright (c) 2022 The CSTAR Lab of WHU. All rights reserved. 
- * 
+ *
+ * @copyright Copyright (c) 2022 The CSTAR Lab of WHU. All rights reserved.
+ *
  * Maybe useful as a stable ABI that can be used by programs that keep mergebot
- * in a shared library, or for a JNI api. 
- * 
- * Some conventions we try to follow: 
- * (1) we expose just opaque struct pointers and functions to clients. This 
- * allows us to change internal representations without having to recompile 
- * clients. 
- * 
- * (2) Errors are represented by a null-terminated c string. NULL means no 
+ * in a shared library, or for a JNI api.
+ *
+ * Some conventions we try to follow:
+ * (1) we expose just opaque struct pointers and functions to clients. This
+ * allows us to change internal representations without having to recompile
+ * clients.
+ *
+ * (2) Errors are represented by a null-terminated c string. NULL means no
  * error. All operations that can raise an error are passwd a "char** errptr" as
- * the last argument. One of the following must be true on entry: 
+ * the last argument. One of the following must be true on entry:
  *   *errptr == NULL
  *   *errptr points to a malloc()ed null-terminated error message
- * On success, a mergebot routine leaves *errptr unchanged. 
- * On failure, mergebot frees the old value of *errptr and set *errptr to a 
- * malloc()ed error message. 
- * 
+ * On success, a mergebot routine leaves *errptr unchanged.
+ * On failure, mergebot frees the old value of *errptr and set *errptr to a
+ * malloc()ed error message.
+ *
  * (3) Bools have the type uint8_t (0 == false; rest == true)
- * 
- * (4) All of the pointer arguments must be non-NULL. 
- * 
+ *
+ * (4) All of the pointer arguments must be non-NULL.
+ *
  */
 
 #define MB_INCLUDE_C_H_
 #define MB_INCLUDE_C_H_
 
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 #ifdef __cplusplus
 }
---
+++ include/utils/LinkedList.h
@@ -0,0 +1,92 @@
+//
+// Created by whalien on 17/11/22.
+//
+
+#ifndef MERGEBOT_LINKEDLIST_H
+#define MERGEBOT_LINKEDLIST_H
+
+#include <algorithm>
+#include <list>
+
+template <typename T>
+class LinkedList : public std::list<T> {
+ public:
+  LinkedList() : std::list<T>() {}
+  explicit LinkedList(size_t len) : std::list<T>(len) {}
+
+  using std::list<T>::back;
+  using std::list<T>::empty;
+  using std::list<T>::front;
+  using std::list<T>::push_back;
+  using std::list<T>::push_front;
+  using std::list<T>::size;
+
+  bool isEmpty() const { return std::list<T>::empty(); }
+
+  void append(const T &t) { std::list<T>::push_back(t); }
+  void append(T &&t) { std::list<T>::push_back(std::move(t)); }
+  void prepend(const T &t) { std::list<T>::push_front(t); }
+  void prepend(T &&t) { std::list<T>::push_front(std::move(t)); }
+
+  T &first() { return std::list<T>::front(); }
+  const T &first() const { return std::list<T>::front(); }
+
+  T &last() { return std::list<T>::back(); }
+  const T &last() const { return std::list<T>::back(); }
+
+  T takeFirst() {
+    assert(!empty());
+    const T t = first();
+    std::list<T>::pop_front();
+    return t;
+  }
+  T takeLast() {
+    assert(!empty());
+    const T t = last();
+    std::list<T>::pop_back();
+    return t;
+  }
+
+  T takeFront() {
+    assert(!empty());
+    const T t = first();
+    std::list<T>::pop_front();
+    return t;
+  }
+  T takeBack() {
+    assert(!empty());
+    const T t = last();
+    std::list<T>::pop_back();
+    return t;
+  }
+
+  bool contains(const T &t) const {
+    return std::find(std::list<T>::begin(), std::list<T>::end(), t) !=
+           std::list<T>::end();
+  }
+
+  typename std::list<T>::iterator find(const T &t) {
+    for (auto it = std::list<T>::begin(); it != std::list<T>::end(); ++it) {
+      if (*it == t) return it;
+    }
+    return std::list<T>::end();
+  }
+
+  typename std::list<T>::const_iterator find(const T &t) const {
+    for (auto it = std::list<T>::begin(); it != std::list<T>::end(); ++it) {
+      if (*it == t) return it;
+    }
+    return std::list<T>::end();
+  }
+
+  void deleteAll() {
+    typename std::list<T>::iterator it = std::list<T>::begin();
+    while (it != std::list<T>::end()) {
+      delete *it;
+      ++it;
+    }
+    std::list<T>::clear();
+  }
+};
+
+#endif  // MERGEBOT_LINKEDLIST_H
