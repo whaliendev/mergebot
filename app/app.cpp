a4ad857be84f31bc1d93111cb07d0100b2cc4801
Hwa <hwahe.cs@gmail.com>
2022-11-29 17:39:19 +0800

    chore: format cxx

--- app/app.cpp
+++ app/app.cpp
@@ -3,10 +3,7 @@
 #include "clp/command_line_parser.h"
 #include "mergebot.h"
 
-int main(int argc, char** argv){
-    MergeBot mb;
-    const mergebot::commandlineparser::ParseStatus status = mb.parse(argc, argv);
-
+int main(int argc, char** argv) {
+  MergeBot mb;
+  const mergebot::commandlineparser::ParseStatus status = mb.parse(argc, argv);
 }
-
-
