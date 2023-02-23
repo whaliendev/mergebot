346ccb42be403203cf430a7bd5e011247e166382
Hwa <hwahe.cs@gmail.com>
2023-02-24 01:15:32 +0800

    feat: add infra for glboal exception handler

--- src/controller/ExceptionHandlerAspect.h
+++ src/controller/ExceptionHandlerAspect.h
@@ -2,7 +2,50 @@
 // Created by whalien on 23/02/23.
 //
 
-#ifndef MB_EXCEPTIONHANDLERWRAPPER_H
-#define MB_EXCEPTIONHANDLERWRAPPER_H
+#ifndef MB_EXCEPTIONHANDLERASPECT_H
+#define MB_EXCEPTIONHANDLERASPECT_H
 
-#endif  // MB_EXCEPTIONHANDLERWRAPPER_H
+#include <exception>
+#include <crow.h>
+#include <fmt/format.h>
+
+#include "app_exception.h"
+
+namespace mergebot {
+namespace server {
+template <typename Func, typename Response = crow::response>
+class ExceptionHandlerAspect {
+ public:
+  ExceptionHandlerAspect(Func f, Response& res) : func_(f), res_(res) {}
+
+  template <typename... Args>
+  auto operator()(Args... args) -> decltype(func_(args...)) {
+    static_assert(
+        std::is_default_constructible<decltype(func_(args...))>::value,
+        "the call result's type of function passed in must be default "
+        "constructible");
+    decltype(func_(args...)) res;
+    try {
+      res = func_(args...);
+    } catch (const AppBaseException& ex) {
+      ex.handle(res_);
+    } catch (const std::exception& ex) {
+      auto buf = fmt::memory_buffer ();
+      fmt::format_to(std::back_inserter(buf),
+                     u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})", "S0000", ex.what(), nullptr);
+      auto body = to_string(buf);
+      crow::response errRes(crow::status::INTERNAL_SERVER_ERROR, body);
+      errRes.set_header("Content-Type", "application/json");
+      res_ = std::move(errRes);
+      res_.end();
+    }
+    return res;
+  }
+
+ private:
+  Func func_;
+  Response& res_;
+};
+}  // namespace server
+}  // namespace mergebot
+#endif  // MB_EXCEPTIONHANDLERASPECT_H
--- src/controller/app_exception.h
+++ src/controller/app_exception.h
@@ -5,16 +5,43 @@
 #ifndef MB_MB_EXCEPTIONS_H
 #define MB_MB_EXCEPTIONS_H
 
+#include <crow.h>
+
 #include <exception>
+#include <string>
+
 #include "../utility.h"
 
 namespace mergebot {
 namespace server {
-class AppBaseException: public std::exception {
+class AppBaseException : public std::exception {
  public:
-  AppBaseException()
+  explicit AppBaseException(std::string code = "S0000",
+                            std::string msg = "MBSA服务端运行时异常")
+      : code_(code), msg_(msg) {}
+
+  const char* what() const noexcept override { return msg_.c_str(); }
+
+  void handle(crow::response& res) const {
+    auto buf = fmt::memory_buffer ();
+    fmt::format_to(std::back_inserter(buf),
+                   u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})", code_, msg_, nullptr);
+    auto body = to_string(buf);
+    auto status = crow::status::OK;
+    if (msg_.length() && msg_[0] == 'C') {
+      status = crow::status::BAD_REQUEST;
+    }
+    crow::response errRes(status, body);
+    errRes.set_header("Content-Type", "application/json");
+    res = std::move(errRes);
+    res.end();
+  }
+
+ private:
+  std::string code_;
+  std::string msg_;
 };
-}
-}
+}  // namespace server
+}  // namespace mergebot
 
 #endif  // MB_MB_EXCEPTIONS_H
