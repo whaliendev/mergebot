11a1d2517baad71d54043e699cdbd07925cdf899
Hwa <hwahe.cs@gmail.com>
2023-02-24 17:11:16 +0800

    feat: add response crafter mw

--- src/controller/response_tuner_mw.h
+++ src/controller/response_tuner_mw.h
@@ -2,27 +2,24 @@
 // Created by whalien on 23/02/23.
 //
 
-#ifndef MB_RESPONSE_CRAFTER_MW_H
-#define MB_RESPONSE_CRAFTER_MW_H
+#ifndef MB_RESPONSE_TUNER_MW_H
+#define MB_RESPONSE_TUNER_MW_H
 
 #include <crow.h>
 #include <fmt/format.h>
 
-namespace mergebot{
+namespace mergebot {
 namespace server {
-struct ResponseCrafter
-{
+struct ResponseTuner {
   struct context {};
 
-  void before_handle(crow::request& req, crow::response& res, context& ctx){
-
-  }
+  void before_handle(crow::request& req, crow::response& res, context& ctx) {}
 
   void after_handle(crow::request& req, crow::response& res, context& ctx) {
-    // response whose body is built with ResultEnum
+    // response whose body is built with Result
     const auto body = res.body;
     crow::json::rvalue bodyRv = crow::json::load(body);
-    if (!bodyRv.has("errorMsg"))    return;
+    if (!bodyRv.has("errorMsg")) return;
     auto errorMsg = static_cast<std::string>(bodyRv["errorMsg"]);
     if (errorMsg.length() && errorMsg[0] == 'C') {
       res.code = crow::status::BAD_REQUEST;
@@ -35,4 +32,4 @@ struct ResponseCrafter
 }  // namespace server
 }  // namespace mergebot
 
-#endif  // MB_RESPONSE_CRAFTER_MW_H
+#endif  // MB_RESPONSE_TUNER_MW_H
