04d7f56d1a807bb5b007b6c96c2130fbcc84a134
Hwa <hwahe.cs@gmail.com>
2023-02-24 17:12:54 +0800

    feat: add global exception handle support

--- src/controller/app_exception.h
+++ src/controller/app_exception.h
@@ -16,24 +16,24 @@ namespace mergebot {
 namespace server {
 class AppBaseException : public std::exception {
  public:
-  explicit AppBaseException(std::string code = "S0000",
-                            std::string msg = "MBSA服务端运行时异常")
-      : code_(code), msg_(msg) {}
+  explicit AppBaseException(mergebot::server::Result err) : code_(err.code), msg_(err.errorMsg) {}
 
   const char* what() const noexcept override { return msg_.c_str(); }
 
   void handle(crow::response& res) const {
-    auto buf = fmt::memory_buffer ();
-    fmt::format_to(std::back_inserter(buf),
-                   u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})", code_, msg_, nullptr);
+    auto buf = fmt::memory_buffer();
+    fmt::format_to(std::back_inserter(buf), u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})",
+                   code_, msg_, nullptr);
     auto body = to_string(buf);
-    auto status = crow::status::OK;
-    if (msg_.length() && msg_[0] == 'C') {
+    auto status = crow::status::INTERNAL_SERVER_ERROR;
+    if (code_.length() && code_[0] == 'C') {
       status = crow::status::BAD_REQUEST;
+    } else if (code_.length() && code_[0] == 'S') {
+      status = crow::status::INTERNAL_SERVER_ERROR;
     }
-    crow::response errRes(status, body);
-    errRes.set_header("Content-Type", "application/json");
-    res = std::move(errRes);
+    res.code = status;
+    res.body = body;
+    res.set_header("Content-Type", "application/json");
     res.end();
   }
 
--- src/controller/exception_handler_aspect.h
+++ src/controller/exception_handler_aspect.h
@@ -2,50 +2,53 @@
 // Created by whalien on 23/02/23.
 //
 
-#ifndef MB_EXCEPTIONHANDLERASPECT_H
-#define MB_EXCEPTIONHANDLERASPECT_H
+#ifndef MB_EXCEPTION_HANDLER_ASPECT_H
+#define MB_EXCEPTION_HANDLER_ASPECT_H
 
-#include <exception>
 #include <crow.h>
 #include <fmt/format.h>
+
+#include <exception>
 
 #include "app_exception.h"
 
 namespace mergebot {
 namespace server {
+
+using CReqMResFuncType = std::function<crow::json::wvalue(const crow::request&, crow::response&)>;
+
 template <typename Func, typename Response = crow::response>
 class ExceptionHandlerAspect {
+ private:
+  Func func_;
+  Response& res_;
+
  public:
   ExceptionHandlerAspect(Func f, Response& res) : func_(f), res_(res) {}
 
   template <typename... Args>
-  auto operator()(Args... args) -> decltype(func_(args...)) {
-    static_assert(
-        std::is_default_constructible<decltype(func_(args...))>::value,
-        "the call result's type of function passed in must be default "
-        "constructible");
+  auto operator()(Args&&... args) -> decltype(func_(args...)) {
+    static_assert(std::is_default_constructible<decltype(func_(args...))>::value,
+                  "the call result's type of function passed in must be default "
+                  "constructible");
     decltype(func_(args...)) res;
     try {
       res = func_(args...);
     } catch (const AppBaseException& ex) {
       ex.handle(res_);
     } catch (const std::exception& ex) {
-      auto buf = fmt::memory_buffer ();
-      fmt::format_to(std::back_inserter(buf),
-                     u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})", "S0000", ex.what(), nullptr);
+      auto buf = fmt::memory_buffer();
+      fmt::format_to(std::back_inserter(buf), u8R"({{"code": "{}", "errorMsg": "{}", "data": {}}})",
+                     "S0000", ex.what(), nullptr);
       auto body = to_string(buf);
-      crow::response errRes(crow::status::INTERNAL_SERVER_ERROR, body);
-      errRes.set_header("Content-Type", "application/json");
-      res_ = std::move(errRes);
+      res_.code = crow::status::INTERNAL_SERVER_ERROR;
+      res_.body = body;
+      res_.set_header("Content-Type", "application/json");
       res_.end();
     }
     return res;
   }
-
- private:
-  Func func_;
-  Response& res_;
 };
 }  // namespace server
 }  // namespace mergebot
-#endif  // MB_EXCEPTIONHANDLERASPECT_H
+#endif  // MB_EXCEPTION_HANDLER_ASPECT_H
