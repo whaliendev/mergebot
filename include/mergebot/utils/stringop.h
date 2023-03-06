0e7307e3d9fcfce8dafb1eb764aeb08d5e654e1b
Hwa <hwahe.cs@gmail.com>
2023-03-06 16:06:44 +0800

    style: format utils/stringop.h

--- include/mergebot/utils/stringop.h
+++ include/mergebot/utils/stringop.h
@@ -10,16 +10,15 @@
 
 namespace mergebot {
 namespace util {
-std::vector<std::string_view> string_split(std::string_view str, std::string_view delims = " ")
-{
+std::vector<std::string_view> string_split(std::string_view str, std::string_view delims = " ") {
   std::vector<std::string_view> output;
-  //output.reserve(str.size() / 2);
+  // output.reserve(str.size() / 2);
 
-  for (auto first = str.data(), second = str.data(), last = first + str.size(); second != last && first != last; first = second + 1) {
+  for (auto first = str.data(), second = str.data(), last = first + str.size();
+       second != last && first != last; first = second + 1) {
     second = std::find_first_of(first, last, std::cbegin(delims), std::cend(delims));
 
-    if (first != second)
-      output.emplace_back(first, second - first);
+    if (first != second) output.emplace_back(first, second - first);
   }
 
   return output;
