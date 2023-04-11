753ef412d5f2e07d36f407a532ffec4f03504a1f
Hwa <hwahe.cs@gmail.com>
2023-04-11 22:06:27 +0800

    feat: make inference from pre-exists CompDB

--- include/mergebot/core/handler/ASTBasedHandler.h
+++ include/mergebot/core/handler/ASTBasedHandler.h
@@ -6,16 +6,32 @@
 #define MB_ASTBASEDHANDLER_H
 
 #include "SAHandler.h"
-#include "mergebot/core/model/ConflictFile.h"
+
+#include <clang/Tooling/CompilationDatabase.h>
+#include <memory>
+#include <spdlog/spdlog.h>
 #include <string>
 #include <vector>
+
+#include "mergebot/core/model/ConflictFile.h"
+#include "mergebot/filesystem.h"
 
 namespace mergebot {
 namespace sa {
 class ASTBasedHandler : public SAHandler {
 public:
   explicit ASTBasedHandler(ProjectMeta Meta, std::string Name = __FILE_NAME__)
-      : SAHandler(Meta, Name) {}
+      : SAHandler(Meta, Name) {
+    OurCompDB = fs::path(Meta.MSCacheDir) / "ours" / CompDBRelative;
+    TheirCompDB = fs::path(Meta.MSCacheDir) / "theirs" / CompDBRelative;
+    BaseCompDB = fs::path(Meta.MSCacheDir) / "base" / CompDBRelative;
+
+    OurDir = fs::path(Meta.MSCacheDir) / "ours";
+    TheirDir = fs::path(Meta.MSCacheDir) / "theirs";
+    BaseDir = fs::path(Meta.MSCacheDir) / "base";
+
+    initCompDB();
+  }
 
 private:
   void
@@ -28,7 +44,26 @@ private:
   bool replaceProjPath(std::string const &CompDBPath,
                        std::string_view ProjPath) const;
 
+  void initCompDB();
+
   const static std::string CompDBRelative;
+
+  // CompDB file
+  std::string OurCompDB;
+  std::string BaseCompDB;
+  std::string TheirCompDB;
+
+  // three revisions source dir
+  std::string OurDir;
+  std::string BaseDir;
+  std::string TheirDir;
+
+  std::pair<std::unique_ptr<clang::tooling::CompilationDatabase>, bool>
+      OurCompilationsPair;
+  std::pair<std::unique_ptr<clang::tooling::CompilationDatabase>, bool>
+      BaseCompilationsPair;
+  std::pair<std::unique_ptr<clang::tooling::CompilationDatabase>, bool>
+      TheirCompilationsPair;
 };
 } // namespace sa
 } // namespace mergebot
--- src/core/handler/ASTBasedHandler.cpp
+++ src/core/handler/ASTBasedHandler.cpp
@@ -7,6 +7,8 @@
 #include "mergebot/filesystem.h"
 #include "mergebot/utils/fileio.h"
 
+#include <clang/Tooling/CompilationDatabase.h>
+#include <clang/Tooling/JSONCompilationDatabase.h>
 #include <filesystem>
 #include <fstream>
 #include <llvm/ADT/StringRef.h>
@@ -28,30 +30,30 @@ void ASTBasedHandler::resolveConflictFiles(
          "ConflictFile sizes should be greater than zero");
 
   spdlog::info("we are resolving conflicts using AST based handler");
-
-  const fs::path OurCompDB =
-      fs::path(Meta.MSCacheDir) / "ours" / CompDBRelative;
-  const fs::path TheirCompDB =
-      fs::path(Meta.MSCacheDir) / "theirs" / CompDBRelative;
-  const fs::path BaseCompDB =
-      fs::path(Meta.MSCacheDir) / "base" / CompDBRelative;
   if (!fs::exists(OurCompDB) || !fs::exists(TheirCompDB) ||
       !fs::exists(BaseCompDB)) {
-    spdlog::warn(
-        "compile commands not provided, we'll skip AST based analysis");
+    spdlog::warn("CompDB doesn't exist, we'll skip AST based handler");
+    return;
+  }
+  replaceProjPath(OurCompDB, OurDir);
+  replaceProjPath(TheirCompDB, TheirDir);
+  replaceProjPath(BaseCompDB, BaseDir);
+
+  //  bool ok1 = OurCompilationsPair.second;
+  auto &[OurCompilations, ok1] = OurCompilationsPair;
+  auto &[BaseCompilations, ok2] = BaseCompilationsPair;
+  auto &[TheirCompilations, ok3] = TheirCompilationsPair;
+  if (!ok1 || !ok2 || !ok3) {
+    spdlog::warn("cannot read CompDB, we'll skip AST based analysis");
     return;
   }
 
-  std::string OurPath = fs::path(Meta.MSCacheDir) / "ours";
-  std::string TheirPath = fs::path(Meta.MSCacheDir) / "theirs";
-  std::string BasePath = fs::path(Meta.MSCacheDir) / "base";
-
-  replaceProjPath(OurCompDB, OurPath);
-  replaceProjPath(TheirCompDB, TheirPath);
-  replaceProjPath(BaseCompDB, BasePath);
-
-  std::vector<std::string> OurConflicts =
-      collectAnalysisFileSet(ConflictFiles, OurPath);
+  std::vector<std::string> OurConflictFiles =
+      collectAnalysisFileSet(ConflictFiles, OurDir);
+  std::vector<std::string> BaseConflictFiles =
+      collectAnalysisFileSet(ConflictFiles, BaseDir);
+  std::vector<std::string> TheirConflictFiles =
+      collectAnalysisFileSet(ConflictFiles, TheirDir);
 }
 
 std::vector<std::string> ASTBasedHandler::collectAnalysisFileSet(
@@ -77,14 +79,15 @@ bool ASTBasedHandler::replaceProjPath(std::string const &CompDBPath,
     ss.write(Buffer, BytesRead);
   }
 
+  // remove trailing separator if it has
   std::string ProjectPath = Meta.ProjectPath;
   if (ProjectPath.back() == fs::path::preferred_separator) {
     ProjectPath.pop_back();
   }
   std::string FileData = ss.str();
-  re2::RE2 OldPath("(" + re2::RE2::QuoteMeta(ProjectPath) + ")");
-  assert(OldPath.ok() && "fail to compile regex for CompDB");
-  int ReplaceCnt = re2::RE2::GlobalReplace(&FileData, OldPath, ProjPath);
+  re2::RE2 OriginalPath("(" + re2::RE2::QuoteMeta(ProjectPath) + ")");
+  assert(OriginalPath.ok() && "fail to compile regex for CompDB");
+  int ReplaceCnt = re2::RE2::GlobalReplace(&FileData, OriginalPath, ProjPath);
 #ifndef NDEBUG
   if (!ReplaceCnt) {
     spdlog::warn("we replaced 0 original project path, which is weird");
@@ -93,6 +96,44 @@ bool ASTBasedHandler::replaceProjPath(std::string const &CompDBPath,
 
   util::file_overwrite_content(CompDBPath, FileData);
   return true;
+}
+
+void ASTBasedHandler::initCompDB() {
+  std::string ErrMsg;
+  OurCompilationsPair.first = clang::tooling::inferMissingCompileCommands(
+      clang::tooling::JSONCompilationDatabase::loadFromFile(
+          OurCompDB, ErrMsg,
+          clang::tooling::JSONCommandLineSyntax::AutoDetect));
+  //));
+  OurCompilationsPair.second = true;
+  if (!ErrMsg.empty()) {
+    spdlog::error(ErrMsg, OurCompDB);
+    OurCompilationsPair.second = false;
+  }
+
+  ErrMsg.clear();
+  BaseCompilationsPair.first = clang::tooling::inferMissingCompileCommands(
+      clang::tooling::JSONCompilationDatabase::loadFromFile(
+          BaseCompDB, ErrMsg,
+          clang::tooling::JSONCommandLineSyntax::AutoDetect));
+  //)
+  BaseCompilationsPair.second = true;
+  if (!ErrMsg.empty()) {
+    spdlog::error(ErrMsg, BaseCompDB);
+    BaseCompilationsPair.second = false;
+  }
+
+  ErrMsg.clear();
+  TheirCompilationsPair.first = clang::tooling::inferMissingCompileCommands(
+      clang::tooling::JSONCompilationDatabase::loadFromFile(
+          OurCompDB, ErrMsg,
+          clang::tooling::JSONCommandLineSyntax::AutoDetect));
+  //)
+  TheirCompilationsPair.second = true;
+  if (!ErrMsg.empty()) {
+    spdlog::error(ErrMsg, TheirCompDB);
+    TheirCompilationsPair.second = false;
+  }
 }
 
 } // namespace sa
