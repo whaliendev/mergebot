0e7335420c8e627ed0df8dc634e8d914d835e022
Hwa <hwahe.cs@gmail.com>
2023-05-04 23:08:24 +0800

    refactor: rename SemanticFollower to RefactorFollower

---
+++ include/mergebot/core/semantic/RefactorFollower.h
@@ -0,0 +1,10 @@
+//
+// Created by whalien on 30/04/23.
+//
+
+#ifndef MB_REFACTORFOLLOWER_H
+#define MB_REFACTORFOLLOWER_H
+
+class RefactorFollower {};
+
+#endif // MB_REFACTORFOLLOWER_H
--- src/core/semantic/SemanticFollower.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by whalien on 30/04/23.
-//
-
-#include "mergebot/core/semantic/SemanticFollower.h"
