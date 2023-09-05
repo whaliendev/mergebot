1e14aeb0c7ec6b2736672873e721beccf9cc4bc5
Hwa <hwahe.cs@gmail.com>
2023-09-05 17:59:23 +0800

    chore: refactor tools/stat.sh

--- tools/stat.sh
+++ tools/stat.sh
@@ -1,1 +1,1 @@
-cloc --vcs git . --exclude-dir=third_party
+cloc --vcs git . --exclude-dir=third_party --exclude-dir=tools --fullpath --not-match-d='tests/mock' --not-match-d='include/mergebot/utils'
