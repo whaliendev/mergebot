e3c1f804abbed74e2d50b076af05b20a63f815f7
Hwa <hwahe.cs@gmail.com>
2023-10-22 16:54:50 +0800

    eva: add log tests

--- eva/log.py
+++ eva/log.py
@@ -2,33 +2,40 @@ import logging
 
 
 class ColorfulFormatter(logging.Formatter):
-    grey = "\x1b[38;20m"
-    yellow = "\x1b[33;20m"
-    red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
-    reset = "\x1b[0m"
-    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    black = "\033[30m"
+    red = "\033[31m"
+    green = "\033[32m"
+    yellow = "\033[33m"
+    blue = "\033[34m"
+    magenta = "\033[35m"
+    cyan = "\033[36m"
+    white = "\033[37m"
+    reset = "\033[0m"
+
+    format_template = "%(asctime)s {}[%(levelname)s]{} (%(filename)s:%(lineno)d): %(message)s"
 
     FORMATS = {
-        logging.DEBUG: grey + format + reset,
-        logging.INFO: grey + format + reset,
-        logging.WARNING: yellow + format + reset,
-        logging.ERROR: red + format + reset,
-        logging.CRITICAL: bold_red + format + reset
+        logging.DEBUG: cyan,
+        logging.INFO: green,
+        logging.WARNING: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: red
     }
 
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
+        record.levelname = record.levelname.lower()
+        level_color = self.FORMATS.get(record.levelno)
+        log_fmt = self.format_template.format(level_color, self.reset)
+        formatter = logging.Formatter(log_fmt, datefmt='[%Y-%m-%d %H:%M:%S]')
         return formatter.format(record)
 
 
-# 配置默认的logger,该logger会同时输出到命令行和文件，命令行中为彩色输出，文件中为无色输出
 # 命令行中的日志级别为INFO，文件中的日志级别为DEBUG
 logger = logging.getLogger(__package__)
 logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    '[%(asctime)s] [%(process)d] [%(levelname)s] (%(filename)s:%(lineno)d): %(message)s')
+
 # 输出到命令行，命令行需要彩色输出
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
