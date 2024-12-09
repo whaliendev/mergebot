284551699cdc53347300615970b31b7ceb1f681a
Hwa <hwahe.cs@gmail.com>
2024-12-09 14:36:04 +0800

    refactor(backend): prefix rest api with /api/manager for nginx routing

--- ui/conflict-manager/src/main/java/com/example/filemanager/controller/ControlController.java
+++ ui/conflict-manager/src/main/java/com/example/filemanager/controller/ControlController.java
@@ -2,26 +2,23 @@ package com.example.filemanager.controller;
 
 import com.example.filemanager.dao.fileInfoMapper;
 import com.example.filemanager.dao.SolveMapper;
-import com.example.filemanager.pojo.fileInfoWithBLOBs;
+import org.springframework.web.bind.annotation.RestController;
 import com.example.filemanager.pojo.Solved;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@org.springframework.web.bind.annotation.RestController
+@CrossOrigin
+@RestController
 public class RestController {
     @Autowired
     private fileInfoMapper fileInfoMapper;
     @Autowired
     private SolveMapper mapper;
-
-//    @GetMapping(value = "/getfile")
-//    public fileInfoWithBLOBs getUser(){
-//        return fileInfoMapper.selectByPrimaryKey("anotherFile.java");
-//    }
 
     @DeleteMapping(value ="/delete")
     public int deleteAll(@RequestParam("repo") String repo){
